655886ee236c2d833fca654264b1504bdac8b5f9
Abner Lucas <abnerlu19@gmail.com>
2024-10-08 17:18:10 -0300

    Desafio realizado com sucesso

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,19 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    // Feito
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+
+        public Nokia(string numero, string modelo, string imei, int memoria) : base (numero,modelo,imei,memoria)
+        {
+            Numero = numero;
+            
+        }
+
+        // Feito
+        public override void InstalarAplicativo(string nomeApp)
+        {
+          Console.WriteLine($"Aplicativo \"{nomeApp}\" instalado com sucesso no Nokia");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Feito
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero:"123456",modelo:"Modelo 1",imei:"11111111", memoria: 128);
+nokia.Ligar(); 
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("---------------------------------");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero:"24689",modelo:"Modelo 2",imei:"2222222", memoria: 64);
+iphone.Ligar(); 
+iphone.InstalarAplicativo("Uber");
